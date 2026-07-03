e622501f78b6b6d31053174b546beb7cfa1e03df
cocosilex <cocosilex@gmail.com>
2026-07-03 16:52:39 +0200

    added compil script (python), removed all pdf from repo

---
+++ compilation/compilation_chapitre.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+from pathlib import Path
+
+CHAPTERS_LOCATION = "./"
+
+BUILD_DIR = CHAPTERS_LOCATION + "build/"
+C_CHAPITRES_DIR = BUILD_DIR + "chapitres/"
+C_COURS_DIR = BUILD_DIR + "cours/"
+C_TD_DIR = BUILD_DIR + "TDs/"
+
+LATEX_COMPILER = "pdflatex"
+GARBAGE_EXTENSIONS = {
+    ".aux", ".log", ".toc", ".out", ".synctex.gz", 
+    ".fls", ".fdb_latexmk", ".lof", ".lot", ".bcf", ".run.xml"
+}
+
+parser = argparse.ArgumentParser(description="Compilation script for MPI math courses.")
+
+parser.add_argument("-ch", "--chapitres", default="all", help="Chapters to compile, default : all.")
+parser.add_argument("-m", "--mode", default="chapitre", help="What to compile, default : chapitre, options : chapitre, cours, TD." )
+
+args = parser.parse_args()
+
+chapters_path = Path(CHAPTERS_LOCATION).resolve()
+
+target_dirs = [
+    d for d in chapters_path.iterdir() 
+    if d.is_dir() and d.name.startswith("chapitre") and d.name != "chapitre0"
+]
+
+if args.chapitres.lower() != "all" and args.chapitres.lower() != "integrale" :
+    # parse args and so smh cool (filter the list)
+    print("ok boy")
+
+build_dir = Path(BUILD_DIR)
+build_dir.mkdir(exist_ok=True)
+
+def clean_dir (dir) :
+    for file in dir.iterdir():
+        if file.is_file() and file.suffix in GARBAGE_EXTENSIONS :
+            file.unlink()
+    return
+
+def compile_file (file, output_dir, cwd_path) :
+    cwd_dir = Path(cwd_path).resolve()
+    
+    result = subprocess.run(
+                            [
+                                "pdflatex",
+                                f"-output-directory={output_dir}",
+                                "-interaction=nonstopmode",
+                                "-halt-on-error",
+                                file
+                            ],
+                            capture_output=True,
+                            cwd=cwd_dir
+                        )
+
+    if(result.returncode != 0) :
+        print("ERROR : Compilation failed for ", file)
+        exit(1)
+    return
+
+def compile_chapters (targets, dir) :
+    for chapitre in targets :
+        chapter_number = int(chapitre.name.replace("chapitre", ""))
+        compile_file(str(chapitre) + "/chapitre" + str(chapter_number) + ".tex", str(dir), str(chapitre))
+
+    return
+
+def compile_cours (targets, dir) :
+    for chapitre in targets :
+        chapter_number = int(chapitre.name.replace("chapitre", ""))
+        compile_file(str(chapitre) + "/cours/" + "cours" + str(chapter_number) + ".tex", str(dir), str(chapitre) + "/cours/")
+        
+    return  
+
+def compile_TDs (targets, dir) :
+    for chapitre in targets :
+        chapter_number = int(chapitre.name.replace("chapitre", ""))
+        compile_file(str(chapitre) + "/TD/" + "TD" + str(chapter_number) + ".tex", str(dir), str(chapitre) + "/TD/")
+
+    return
+
+match args.mode :
+    case "chapitre" :
+        c_chapters_dir = Path(C_CHAPITRES_DIR).resolve()
+        c_chapters_dir.mkdir(exist_ok=True)
+        
+        compile_chapters(target_dirs, c_chapters_dir)
+        compile_chapters(target_dirs, c_chapters_dir)
+        
+        clean_dir(c_chapters_dir)
+    case "cours" :
+        c_cours_dir = Path(C_COURS_DIR).resolve()
+        c_cours_dir.mkdir(exist_ok=True)
+        
+        compile_cours(target_dirs, c_cours_dir)
+        compile_cours(target_dirs, c_cours_dir)
+
+        clean_dir(c_cours_dir)
+    case "TD" :
+        c_TDs_dir = Path(C_TD_DIR).resolve()
+        c_TDs_dir.mkdir(exist_ok=True)
+        
+        compile_TDs(target_dirs, c_TDs_dir)
+        compile_TDs(target_dirs, c_TDs_dir)
+
+        clean_dir(c_TDs_dir)
+    case _ :
+        print("ERROR: invalid input for mode field.")
+        
+print("Compilation finished, pdf are in the build subdir.")
